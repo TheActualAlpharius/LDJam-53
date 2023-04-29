58ed73dea0895bfbeedd343d539899a4d0582c89
eliconstantopoulos <kakoueee@gmail.com>
2023-04-29 12:28:44 +0100

    fixed gamebreaking error

--- Assets/Scripts/Foods/Drag.cs
+++ Assets/Scripts/Foods/Drag.cs
@@ -9,10 +9,10 @@ public class Drag : MonoBehaviour
     private Camera _cam;
     [SerializeField] private float _speed = 50; //speed at which obj follows mouse
     [SerializeField] private float maxSpeed = 10f;
-    private Rigidbody2D rigidbody;
+    private Rigidbody2D rigidBody;
     private void Awake(){
         _cam = Camera.main;
-        rigidbody = GetComponent<Rigidbody2D>();
+        rigidBody = GetComponent<Rigidbody2D>();
 
     }
 
@@ -21,10 +21,10 @@ public class Drag : MonoBehaviour
     }
 
     private void OnMouseDrag(){
-        //rigidbody.position = Vector3.MoveTowards(rigidbody.position, _cam.ScreenToWorldPoint(Input.mousePosition) - difference, _speed * Time.deltaTime);
-        rigidbody.velocity = (_cam.ScreenToWorldPoint(Input.mousePosition) - (Vector3)rigidbody.position - offset) * _speed;
-        float magnitude = rigidbody.velocity.magnitude;
-        rigidbody.velocity = rigidbody.velocity * Mathf.Min(magnitude, maxSpeed) / magnitude;
+        //rigidBody.position = Vector3.MoveTowards(rigidBody.position, _cam.ScreenToWorldPoint(Input.mousePosition) - difference, _speed * Time.deltaTime);
+        rigidBody.velocity = (_cam.ScreenToWorldPoint(Input.mousePosition) - (Vector3)rigidBody.position - offset) * _speed;
+        float magnitude = rigidBody.velocity.magnitude;
+        rigidBody.velocity = rigidBody.velocity * Mathf.Min(magnitude, maxSpeed) / magnitude;
         
 
     }
