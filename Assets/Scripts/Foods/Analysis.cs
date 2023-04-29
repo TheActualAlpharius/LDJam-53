6bf6a192fe5087e5558a80a3a296c673809580ce
eliconstantopoulos <kakoueee@gmail.com>
2023-04-29 20:59:06 +0100

    liver analysis

--- Assets/Scripts/Foods/Analysis.cs
+++ Assets/Scripts/Foods/Analysis.cs
@@ -8,10 +8,6 @@ public class Analysis : MonoBehaviour {
     private void OnTriggerStay2D(Collider2D other){
         Debug.Log("Collision!!");
         Nutrient nutrient = other.gameObject.GetComponent<Nutrient>();
-        if(nutrient == null) return;
-        nutrient.analysisTime -= Time.deltaTime;
-        if(nutrient.analysisTime >= 0) return;
-        //destroy that shit
-        Debug.Log("timor");
+        nutrient.isDissolving = true;
     }
 }
--- Assets/Scripts/Foods/Nutrient.cs
+++ Assets/Scripts/Foods/Nutrient.cs
@@ -5,17 +5,18 @@ using UnityEngine;
 public class Nutrient : MonoBehaviour {
     public bool isGood;
     [SerializeField] public float analysisTime = 3f;
+    [SerializeField] private GameObject childPrefab;
     public bool isDissolving = false;
-    public Nutrient(bool good){
-        isGood = good;
-    }
 
     public void Update(){
         if(isDissolving){
             analysisTime -= Time.deltaTime;
             if(analysisTime <= 0){ 
             //destroy that shit
-            Debug.Log("timor");
+            Debug.Log("Destructo");
+            GameObject child = ObjectPool.GetObject(childPrefab);
+            child.transform.position = gameObject.transform.position;
+            ObjectPool.ReleaseToPool(gameObject);
             }
         }
     }
