d0db0775657b226a1d5feb8faa736b0ab2d48034
volcanoeyes <jiachen.wang2000@gmail.com>
2023-04-29 16:59:51 +0800

    fixed dragging script somewhat

--- Assets/Drag.cs
+++ Assets/Drag.cs
@@ -5,38 +5,40 @@ using UnityEngine;
 public class Drag : MonoBehaviour
 {
 
-    private Vector3 difference = Vector3.zero;
+    private Vector3 offset = Vector3.zero;
     private Vector3 lastPosition = Vector3.zero;
     private Camera _cam;
     private Vector3 trailingDir = Vector3.zero; //direction obj is travelling OnMouseUp
     [SerializeField] private float _speed = 50; //speed at which obj follows mouse
     [SerializeField] private float friction = 0.01f; //how much sliding happens when mouse is released
-
+    private Vector3 rigidBodyVelocity;
+    private Rigidbody2D rigidbody;
     private void Awake(){
         _cam = Camera.main;
+        rigidbody = GetComponent<Rigidbody2D>();
+
     }
 
     private void OnMouseDown(){
         trailingDir = Vector3.zero;
-        difference = _cam.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        offset = _cam.ScreenToWorldPoint(Input.mousePosition) - transform.position;
     }
 
     private void OnMouseDrag(){
-        Debug.Log(transform.position == lastPosition);
-        lastPosition = transform.position;
-        transform.position = Vector3.MoveTowards(transform.position, _cam.ScreenToWorldPoint(Input.mousePosition) - difference, _speed * Time.deltaTime);
+        lastPosition = rigidbody.position;
+        //rigidbody.position = Vector3.MoveTowards(rigidbody.position, _cam.ScreenToWorldPoint(Input.mousePosition) - difference, _speed * Time.deltaTime);
+        Debug.Log((Vector3)rigidbody.position);    
+        rigidbody.velocity = (_cam.ScreenToWorldPoint(Input.mousePosition) - (Vector3)rigidbody.position - offset) * _speed;
     }
 
     private void OnMouseUp(){
-        trailingDir = transform.position - lastPosition;
+        rigidbody.velocity = transform.position - lastPosition;
 
     }
 
-    private void Update(){
-        Debug.Log(trailingDir);
-        if (trailingDir.magnitude > 0){
-            transform.position += trailingDir;
-            trailingDir -= trailingDir * friction;
+    private void FixedUpdate(){
+        if (rigidbody.velocity.magnitude > 0){
+            //rigidbody.velocity -= rigidbody.velocity * friction;
         }
 
     }
