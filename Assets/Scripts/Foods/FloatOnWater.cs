f7a3481c6a1d1aca8e20fcf73cbd0f3dae731f9e
volcanoeyes <130888597+volcanoeyes@users.noreply.github.com>
2023-05-01 00:04:31 +0800

    made spawner work again

--- Assets/Scripts/Foods/FloatOnWater.cs
+++ Assets/Scripts/Foods/FloatOnWater.cs
@@ -4,7 +4,4 @@ using UnityEngine;
 
 public class FloatOnWater : MonoBehaviour
 {
-    private void OnEnable(){
-        transform.position = new Vector3(7.5f, 38f);
-    }
 }
--- Assets/Scripts/Foods/Food.cs
+++ Assets/Scripts/Foods/Food.cs
@@ -6,12 +6,20 @@ public class Food : MonoBehaviour
 {
     private SpriteRenderer spriteRenderer;
     [SerializeField] private Sprite[] foodSprites;
+    [SerializeField] private Sprite[] nonFoodSprites;
+    public bool notFood;
     private void OnEnable(){
         transform.position = new Vector3(7.5f, 38f);
         spriteRenderer = GetComponentInChildren<SpriteRenderer>();
         Debug.Log(spriteRenderer.sprite);
-        int randomIndex = Random.Range(0, foodSprites.Length);
-        spriteRenderer.sprite = foodSprites[randomIndex];
+        int randomIndex = Random.Range(0, foodSprites.Length + nonFoodSprites.Length);
+        if (randomIndex < foodSprites.Length){
+            notFood = false;
+            spriteRenderer.sprite = foodSprites[randomIndex];
+        }else{
+            notFood = true;
+            spriteRenderer.sprite = nonFoodSprites[randomIndex % foodSprites.Length];
+        }
         
 
 
